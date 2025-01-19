7cf5d231e3180ea641e120f3d6a84497df3880df
alekseyHunter <okhotnichenko.a.v@gmail.com>
2025-01-19 23:08:56 +0500

    Updated module for game main menu

--- app/src/main/java/my/apple/snake/ui/screens/menu/MenuScreen.kt
+++ app/src/main/java/my/apple/snake/ui/screens/menu/MenuScreen.kt
@@ -2,8 +2,11 @@ package my.apple.snake.ui.screens.menu
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import my.apple.snake.ui.screens.menu.views.MenuViewDisplay
 
 @Composable
 fun MenuScreen(navController: NavController) {
-
+    MenuViewDisplay {
+        navController.navigate(it)
+    }
 }
