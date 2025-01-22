fd5223fae9008d395bc7a29bdcf0850d189c754c
alekseyHunter <okhotnichenko.a.v@gmail.com>
2025-01-22 17:11:15 +0500

    Fixed errors

--- app/src/main/java/my/apple/snake/base/MainActivity.kt
+++ app/src/main/java/my/apple/snake/base/MainActivity.kt
@@ -3,18 +3,16 @@ package my.apple.snake.base
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.navigation.compose.rememberNavController
 import my.apple.snake.ui.navigation.NavHostScreen
-import my.apple.snake.ui.screens.game.GameScreen
 import my.apple.snake.ui.theme.AppleSnakeTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
             AppleSnakeTheme {
